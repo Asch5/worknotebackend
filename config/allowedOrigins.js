28f8fbcb8dc67473f12815750d9a68e257acda24
Asch5 <9106543@gmail.com>
2024-01-08 15:45:12 +0100

    Redy to deploy backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+// const allowedOrigins = [
+//     'http://localhost:3000',
+//     'https://www.dandrepairshop.com',
+//     'https://dandrepairshop.com',
+//     'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd',
+// ];
+
+const allowedOrigins = ['https://worknotes.onrender.com/'];
+
+module.exports = allowedOrigins;
---
+++ middleware/verifyJWT.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    // const cookies = req.cookies;
+
+    // console.log('verifyJWT - authHeader', authHeader);
+    // console.log('verifyJWT - cookies', cookies);
+
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({
+            message: 'Unauthorized in verifyJWT (There is not the authHeader)',
+        });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err)
+            return res.status(403).json({ message: 'Forbidden in verifyJWT' });
+        req.user = decoded.UserInfo.username;
+        req.roles = decoded.UserInfo.roles;
+        next();
+    });
+};
+
+module.exports = verifyJWT;
