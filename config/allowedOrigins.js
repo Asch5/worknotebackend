be6e47a6138936d61f74a8cd9aa6a4a069b646f9
Asch5 <9106543@gmail.com>
2024-01-10 15:23:24 +0100

    initial state

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,11 @@
-// const allowedOrigins = [
-//     'http://localhost:3000',
-//     'https://www.dandrepairshop.com',
-//     'https://dandrepairshop.com',
-//     'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd',
-// ];
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com',
+    'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd',
+    'https://worknotes.onrender.com',
+];
 
-const allowedOrigins = ['https://worknotes.onrender.com'];
+// const allowedOrigins = ['https://worknotes.onrender.com'];
 
 module.exports = allowedOrigins;
